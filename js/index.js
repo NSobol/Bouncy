b204b171aabd6d338b60b287764d379c6d7cf0be
Soboleva Natalia <nsoboleva1979@rambler.ru>
2021-08-16 22:35:57 +0300

    add js and links for menu

--- js/index.js
+++ js/index.js
@@ -6,15 +6,22 @@ let slider = Array.from(sliders);
 let slideIndex = slider.findIndex(elem => elem.classList.contains("blog-item-active"));
 dotsArray[slideIndex].classList.toggle("bullet2-dot-active");
 
-function getRotator(slideIndex) {
-    slider[slideIndex].classList.toggle("blog-item-active");
-    dotsArray[slideIndex].classList.toggle("bullet2-dot-active");
+function getRotator(num) {
+    slider[num].classList.toggle("blog-item-active");
+    dotsArray[num].classList.toggle("bullet2-dot-active");
 }
 
-function getSlider(slideIndex) {
-    getRotator(index);
-    index = index === arrayRorators.length - 1 ? 0 : index + 1;
-    getRotator(index);
+for (let i = 0; i < dotsArray.length; i++) {
+    dotsArray[i].onclick = function () {
+        getRotator(i);
+        slideIndex = slider.findIndex(elem => elem.classList.contains("blog-item-active"));
+        getRotator(slideIndex);
+    }
 }
 
-setInterval(getSlider, 2000);
+function autoSlider() {
+    getRotator(slideIndex);
+    slideIndex = slideIndex === slider.length - 1 ? 0 : slideIndex + 1;
+    getRotator(slideIndex);
+}
+setInterval(autoSlider, 3000);
